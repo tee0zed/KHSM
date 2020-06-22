980393f850e42892bd966cb119a0a0f4901d2377
tee0zed <tee@zed@gmail.com>
2020-06-22 22:18:55 +0300

    initial

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+#
+# Контроллер, отображающий список и профиль юзера
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
---
+++ spec/models/game_question_spec.rb
@@ -0,0 +1,50 @@
+
+require 'rails_helper'
+
+# Тестовый сценарий для модели игрового вопроса, в идеале весь наш функционал
+# (все методы) должны быть протестированы.
+RSpec.describe GameQuestion, type: :model do
+  # Задаем локальную переменную game_question, доступную во всех тестах этого
+  # сценария: она будет создана на фабрике заново для каждого блока it,
+  # где она вызывается.
+  let(:game_question) do
+    FactoryBot.create(:game_question, a: 2, b: 1, c: 4, d: 3)
+  end
+
+  let(:bad_game_question) do
+    FactoryGirl.create(:bad_game_question, a: 2, b: 2, c: 4, d: 3)
+  end
+
+  # Группа тестов на игровое состояние объекта вопроса
+  context 'game status' do
+
+    # Тест на правильную генерацию хэша с вариантами
+    it 'correct .variants' do
+      expect(game_question.variants).to eq(
+        'a' => game_question.question.answer2,
+        'b' => game_question.question.answer1,
+        'c' => game_question.question.answer4,
+        'd' => game_question.question.answer3
+      )
+    end
+
+    # Тест на наличие делегатов level и text
+    it 'correct .level & .text delegates' do
+      expect(game_question.text).to eq(game_question.question.text)
+      expect(game_question.level).to eq(game_question.question.level)
+    end
+
+    it 'correct .answer_correct?' do
+      # Именно под буквой b в тесте мы спрятали указатель на верный ответ
+      expect(game_question.answer_correct?('b')).to be_truthy
+    end
+
+    it 'correct .correct_answer_key' do
+      expect(game_question.correct_answer_key).to eq 'b'
+    end
+
+    it 'bad .correct_answer_key' do
+      expect(bad_game_question.correct_answer_key).to be_nil
+    end
+  end
+end
