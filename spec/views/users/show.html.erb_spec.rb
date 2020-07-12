8b3545b476dafd8472e0c7a7fcc836feade153c1
tee0zed <tee@zed@gmail.com>
2020-07-12 16:18:09 +0300

    show view test fix

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe 'users/show', type: :view do
   context 'viewed by foreign user' do
     before(:each) do
-      current_user = assign(:user, FactoryBot.build_stubbed(:user, name: 'Вася Пупкин'))
-      allow(view).to receive(:current_user).and_return(current_user)
+      current_user = FactoryBot.create(:user, name: 'Вася Пупкин')
+      assign(:user, current_user)
+      sign_in current_user
 
       render
     end
@@ -13,7 +14,7 @@ RSpec.describe 'users/show', type: :view do
       expect(rendered).to match 'Вася Пупкин'
     end
 
-    it 'doesnt render change password button' do
+    it 'renders change password button' do
       expect(rendered).to match 'Сменить имя и пароль'
     end
 
@@ -33,8 +34,20 @@ RSpec.describe 'users/show', type: :view do
       render
     end
 
-    it 'renders change password button' do
+    it 'renders user name' do
+      expect(rendered).to match 'Вася Пупкин'
+    end
+
+    it 'doesn\'t renders change password button' do
       expect(rendered).not_to match 'Сменить имя и пароль'
+    end
+
+    it 'renders game partial' do
+      assign(:games, [FactoryBot.build_stubbed(:game)])
+      stub_template 'users/_game.html.erb' => "User game goes here"
+
+      render
+      expect(rendered).to match "User game goes here"
     end
   end
 end
